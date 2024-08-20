43dc0a3324112398a24052af166604d19ca03d53
SarathKalarikkal <sarathkalarikkal0.com>
2024-08-20 08:36:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    this.classList.toggle('is-active');
+});
+
+
+var initSwiper = function() {
+
+    var swiper = new Swiper(".main-swiper", {
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var bestselling_swiper = new Swiper(".bestselling-swiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      speed: 500,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+      }
+    });
+
+    var testimonial_swiper = new Swiper(".testimonial-swiper", {
+      slidesPerView: 1,
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var products_swiper = new Swiper(".products-carousel", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      speed: 500,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+
+      }
+    });
+
+  }
